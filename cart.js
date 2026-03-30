b2b498746711d36d580bd9cb334991282764ed9a
BilalBennour7 <bilalbennour1@gmail.com>
2026-03-30 12:39:44 -0400

    homework 2

--- cart.js
+++ cart.js
@@ -2,25 +2,44 @@ const Carticon= document.querySelector(".cart-icon");
 const Cart=document.querySelector(".cartTab");
 const Cartclose=document.querySelector(".x-btn");
 const overlay=document.querySelector(".overlay");
-const emptyMessage = document.querySelector(".empty-message");
-const listCart = document.querySelector(".listCart");
-
-function updateEmptyMessage() {
-  if (listCart.children.length === 0) {
-    emptyMessage.style.display = "block";
-  } else {
-    emptyMessage.style.display = "none";
-  }
-}
+const Addcartbuttons=document.querySelectorAll(".add-to-cart");
 
 Carticon.addEventListener("click", () => {
     Cart.classList.add("active");
     overlay.classList.add("active");
 });
+
 Cartclose.addEventListener("click", () => {
     Cart.classList.remove("active");
     overlay.classList.remove("active");
 });
 
+Addcartbuttons.forEach(button => {
+    button.addEventListener("click", event=>{
+        const Menuitem=event.target.closest(".menu-item");
+        Addtocart(Menuitem);
+    });
+});
 
+const cartContent= document.querySelector(".listCart")
+const Addtocart = Menuitem => {
+    const Itemname=Menuitem.querySelector(".item-name").textContent;
+    const ProductPrice=Menuitem.querySelector(".item-price").textContent;
 
+    const Cartbox=document.createElement("div");
+
+    Cartbox.classList.add("cart-box");
+    Cartbox.innerHTML=`
+    <div class="cart-detail">
+    <h2 class="cart-product-title">${Itemname}</h2>
+              <span class="cart-price">${ProductPrice}</span>
+              <div class="cart-quanity">
+                <button id="decerment">-</button>
+                <span class="number">1</span>
+                <button id="increment">+</button>
+              </div>
+            </div>
+            <i class="fa-regular fa-trash-can cart-remove"></i>
+    `;
+    cartContent.appendChild(Cartbox);
+};
