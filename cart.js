a534e2d648667adfeb4183e561a829e0038a9ccf
BilalBennour7 <bilalbennour1@gmail.com>
2026-03-30 11:23:33 -0400

    homework 2

--- cart.js
+++ cart.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+const Carticon= document.querySelector(".cart-icon");
+const Cart=document.querySelector(".cartTab");
+const Cartclose=document.querySelector(".x-btn");
+const overlay=document.querySelector(".overlay");
+const emptyMessage = document.querySelector(".empty-message");
+const listCart = document.querySelector(".listCart");
+
+function updateEmptyMessage() {
+  if (listCart.children.length === 0) {
+    emptyMessage.style.display = "block";
+  } else {
+    emptyMessage.style.display = "none";
+  }
+}
+
+Carticon.addEventListener("click", () => {
+    Cart.classList.add("active");
+    overlay.classList.add("active");
+});
+Cartclose.addEventListener("click", () => {
+    Cart.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+
+
